b7826ebed9eb3c7f3f5a579a3a9c32e64bb652e0
Arthur Ritzel <arthurritzel@users.noreply.github.com>
2023-04-22 15:59:36 -0300

    script

---
+++ script.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.head5', { delay: 500 });
+ScrollReveal().reveal('.head4', { delay: 400 });
+ScrollReveal().reveal('.head3', { delay: 300 });
+ScrollReveal().reveal('.head2', { delay: 200 });
