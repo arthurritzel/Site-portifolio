174a877ee0e9ec150d405a25ea287eec59a0f437
Arthur Ritzel <arthurritzel@users.noreply.github.com>
2023-07-27 15:35:13 -0300

    sim

--- script.js
+++
@@ -1,4 +0,0 @@
-ScrollReveal().reveal('.head5', { delay: 500 });
-ScrollReveal().reveal('.head4', { delay: 400 });
-ScrollReveal().reveal('.head3', { delay: 300 });
-ScrollReveal().reveal('.head2', { delay: 200 });
